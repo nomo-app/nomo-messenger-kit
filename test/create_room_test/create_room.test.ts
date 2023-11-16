b4b352dbcf2d0d7705e4e00a52867108cd930f6e
nomo-fe <dev0@nomo.app>
2023-11-16 15:02:12 +0100

    add a few expect

--- test/create_room_test/create_room.test.ts
+++ test/create_room_test/create_room.test.ts
@@ -1,21 +1,23 @@
-import { nomoRegisterOrLogin, UserMatrix, } from "../../src";
+import { nomoRegisterOrLogin, UserMatrix } from "../../src";
 import { nomoCreateRoom } from "../../src/create_room/create_room";
 import { generateRandomString } from "../test_util";
 
 test("create room", async () => {
-    const mnemonic = "diet say develop title sibling steel blast table chicken foster fuel giraffe";
-    const userMatrix: UserMatrix = await nomoRegisterOrLogin(mnemonic);
-    const args: Map<string, any> = new Map
-    {
-        [
-            ['name', generateRandomString(25)],
-            ["preset", "private_chat",],
-            ['room_alias_name', generateRandomString(10)]
-        ]
-    };
-    let roomId = await nomoCreateRoom(args, userMatrix.access_token);
-    console.log(roomId);
-    if (roomId.length == 0) {
-        fail("no roomId");
-    }
+  const mnemonic =
+    "diet say develop title sibling steel blast table chicken foster fuel giraffe";
+  const userMatrix: UserMatrix = await nomoRegisterOrLogin(mnemonic);
+  const args: Map<string, any> = new Map();
+  {
+    [
+      ["name", generateRandomString(25)],
+      ["preset", "private_chat"],
+      ["room_alias_name", generateRandomString(10)],
+    ];
+  }
+  let roomId = await nomoCreateRoom(args, userMatrix.access_token);
+  console.log("roomId", roomId);
+
+  expect(roomId.length).toBeGreaterThan(0);
+  expect(roomId.startsWith("!")).toBe(true);
+  expect(roomId.endsWith(":zeniq.chat")).toBe(true);
 });
