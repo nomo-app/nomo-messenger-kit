1e3d66946693bc4685f59a17b8be754f5249c55d
dev2-nomo <dev2@nomo.app>
2023-11-20 16:50:01 +0100

    added matrix sdk for encryption

--- test/send_message_test/encrypted/send_encrypted_message.test.ts
+++ test/send_message_test/encrypted/send_encrypted_message.test.ts
@@ -3,14 +3,54 @@ import {
     nomoRegisterOrLogin,
     UserMatrix,
     SendMessageArgs,
+    server,
 } from "../../../src";
 import { nomoCreateRoom } from "../../../src/create_room/create_room";
 import { generateRandomString, getCipherText } from "../../test_util";
+
+// const Olm = require('@matrix-org/olm');
+
+// import matrix from 'matrix-js-sdk';
 
 // The test will create a new room with an initial state which declares the encryption for the room.
 // Then the plain text is encrypted and sent to the room.
 // TODO 
 test("send encrypted message", async () => {
+    // with sdk
+    // await Olm.init();
+    // const mnemonic =
+    //     "diet say develop title sibling steel blast table chicken foster fuel giraffe";
+    // const userMatrix: UserMatrix = await nomoRegisterOrLogin(mnemonic);
+    // let clientData: matrix.ICreateClientOpts = {
+    //     baseUrl: server,
+    //     accessToken: userMatrix.access_token,
+    //     userId: userMatrix.user_id,
+    // };
+
+    // const client = matrix.createClient(clientData);
+    // await client.initCrypto();
+    // await client.startClient();
+
+    // let roomId: any = await client.createRoom({
+    //     invite: ["@example:matrix.org"],
+    //     is_direct: true,
+    //     initial_state: [{
+    //         type: "m.room.encryption",
+    //         state_key: "",
+    //         content: {
+    //             algorithm: "m.megolm.v1.aes-sha2"
+    //         }
+    //     }]
+    // });
+    // console.log("roomId", roomId);
+
+    // let eventId: any = await client.sendEvent(roomId, "m.room.message", {
+    //     body: "Hello, this is an encrypted message!",
+    //     msgtype: "m.text"
+    // });
+    // console.log("eventId", eventId);
+
+    // without sdk
     const mnemonic =
         "diet say develop title sibling steel blast table chicken foster fuel giraffe";
     const userMatrix: UserMatrix = await nomoRegisterOrLogin(mnemonic);
